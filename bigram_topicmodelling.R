c9b9ac6a29a7ab34fd35247245e593707d690cfc
gustav <guslu389@student.liu.se>
2020-08-24 13:52:08 +0200

    separated bigram modelling to use all cores

---
+++ bigram_topicmodelling.R
@@ -0,0 +1,26 @@
+suppressPackageStartupMessages(require(udpipe))
+suppressPackageStartupMessages(require(stringr))
+suppressPackageStartupMessages(require(tidyverse))
+suppressPackageStartupMessages(require(magrittr))
+suppressPackageStartupMessages(require(tm))
+suppressPackageStartupMessages(require(topicmodels))
+suppressPackageStartupMessages(require(parallel))
+suppressPackageStartupMessages(require(textmineR))
+
+source("story_functions.R")
+load(file = "annotated_text.RData")
+set.seed(141114)
+ntopics       <- seq.int(from = 4, to = 32, by = 2)
+
+
+bigram_data   <- expand_grid(ntopics = ntopics,
+                             types = list(c("NOUN", "Bigrams")))
+
+raw_bigram_models <- mclapply(X = 1:nrow(bigram_data),
+                          FUN = function(x){
+                            LDA_ngram_modeller(data = ud_raw,
+                                               types = unlist(bigram_data$types[x]),
+                                               ntopics = bigram_data$ntopics[x])
+                          }, mc.cores = 1L)
+
+save(raw_bigram_models, bigram_data, ntopics, file = "LDA_bigram_models.RData")
