b068e2c5faacb186b8f73936ac6d883ebc224a59
gustav <guslu389@student.liu.se>
2020-08-24 22:29:16 +0200

    REmoved cleaned text from annotation and removed episode naming ppart

--- story_annotation.R
+++ story_annotation.R
@@ -14,13 +14,14 @@ suppressPackageStartupMessages(require(textmineR))
 source("story_functions.R")
 load("episode_download.Rdata")
 
-episode_names <- episode_namer(episode_titles)
+#episode_names <- episode_namer(episode_titles)
 
-episode_names %<>% mutate(story_count = unlist(sapply(episodes, function(x){ifelse(is.null(nrow(x)),0,nrow(x))})))
+#episode_names %<>% mutate(story_count = unlist(sapply(episodes, function(x){ifelse(is.null(nrow(x)),0,nrow(x))})))
 
 #udsv <- udpipe_download_model(language = "swedish") #uncomment to download latest annotation model
 udsv <- udpipe_load_model("swedish-talbanken-ud-2.4-190531.udpipe")
 
+# Cleaning out duplicate stories, and merging multi-blockquote-stories
 episode_data <- bind_rows(episodes) %>% 
   filter(!str_detect(raw_text, "pic\\.twitter\\.com")) %>% 
   mutate(story_words = as.integer(story_words + 1)) %>%  # Eftersom det görs genom att räkna mellansteg 
@@ -39,11 +40,11 @@ episode_data <- bind_rows(episodes) %>%
   story_merger(episode = "E118", start_id = 4, end_id = 7) %>% # SCP-106
   story_merger(episode = "E130", start_id = 3) %>%  #den löånga flygresan i fyra delar
   filter(story_words > 30) %>% 
-  filter(!(story_id %in% c("E89_S2")))
+  filter(!(story_id %in% c("E89_S2", "E67_S8"))) #Mailsvar samt dublett
 
 stoppord <- utf8::as_utf8(unname(unlist(read.csv("stoppord-mycket.csv", stringsAsFactors = FALSE))), normalize = TRUE)
 
-episode_data %<>% mutate(cleaned_text = clean_text(raw_text, stoppord),
+episode_data %<>% mutate(#cleaned_text = clean_text(raw_text, stoppord),
                          raw_text = utf8::as_utf8(str_remove_all(raw_text, "[“”‘’…´]"),TRUE)) 
 
 ud_raw <- udpipe_annotate(udsv, parallel.cores = 3L,
@@ -51,11 +52,11 @@ ud_raw <- udpipe_annotate(udsv, parallel.cores = 3L,
                            doc_id = episode_data$story_id)
 ud_raw <- as.data.frame(ud_raw)
 ud_raw$topic_level_id <- unique_identifier(ud_raw, fields = c("doc_id", "paragraph_id", "sentence_id"))
+# Since only raw texts are used, removing the annotations of cleaned ones.
+# ud_clean <- udpipe_annotate(udsv, parallel.cores = 3L,
+#                           x = episode_data$cleaned_text, 
+#                           doc_id = episode_data$story_id)
+# ud_clean <- as.data.frame(ud_clean)
+# ud_clean$topic_level_id <- unique_identifier(ud_clean, fields = c("doc_id", "paragraph_id", "sentence_id"))
 
-ud_clean <- udpipe_annotate(udsv, parallel.cores = 3L,
-                          x = episode_data$cleaned_text, 
-                          doc_id = episode_data$story_id)
-ud_clean <- as.data.frame(ud_clean)
-ud_clean$topic_level_id <- unique_identifier(ud_clean, fields = c("doc_id", "paragraph_id", "sentence_id"))
-
-save(ud_raw, ud_clean, file = "annotated_text.RData")
+save(ud_raw, file = "annotated_text.RData")
