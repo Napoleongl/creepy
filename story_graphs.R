dbf436b6c0e81797152fc3563603ad908d2d70d1
gustav <guslu389@student.liu.se>
2020-08-24 21:58:00 +0200

    tidyd up coherence graph

--- story_graphs.R
+++ story_graphs.R
@@ -22,6 +22,15 @@ ggplot(episode_data) +
 )
 
 
+# Example data for data chapter
+set.seed(141114)
+episode_data[sample(1:543,5),c(1,5,6)] %>% 
+  mutate(raw_text = paste0(substr(raw_text,1,200),"...")) %>% 
+  xtable(digits =0,caption = "Example of episode data prior to annotation. Only the first 200 characters of the raw\\_text-field are shown") %>% 
+  print.xtable(booktabs = TRUE, caption.placement = "top")
+
+
+
 # Plot for comparing models
 word_type_colours <- RColorBrewer::brewer.pal(length(levels(model_data$word_types))+1, "RdGy")[-c(4)]
 names(word_type_colours) <- levels(model_data$word_types)
@@ -41,10 +50,24 @@ ggsave(filename = "images/model_comp.pdf", width = 10, height =4, units ="in",
          theme (legend.position = "right") 
 )
 
-# Example data for data chapter
-set.seed(141114)
-episode_data[sample(1:543,5),c(1,5,6)] %>% 
-  mutate(raw_text = paste0(substr(raw_text,1,200),"...")) %>% 
-  xtable(digits =0,caption = "Example of episode data prior to annotation. Only the first 200 characters of the raw\\_text-field are shown") %>% 
-  print.xtable(booktabs = TRUE, caption.placement = "top")
-
+# GRaph of topics and coherence
+bar_cols = setNames(c(word_type_colours[1], word_type_colours[3], word_type_colours[5]),
+                    c("coherence", "prevalence", "outl"))
+topic_plot_height <- max(model_data[best_model_idx, "ntopics"]/4+2, 4.5)
+ggsave(filename = "images/toptopics.pdf", width = 10, height = topic_plot_height, units ="in",
+       plot =
+         tibble(coherence = models[[best_model_idx]]$coherence,
+                prevalence = models[[best_model_idx]]$prevalence/100,
+                topic_text = apply(top_topics[[best_model_idx]], 2 ,paste, collapse = ", ")) %>%
+         pivot_longer(-topic_text, names_to = "measure", values_to = "value") %>% 
+         
+         ggplot()+
+         aes(x=topic_text, y=value, group=measure, fill = measure) +
+         geom_col(width = rel(0.15), size = rel(0.1), colour = bar_cols["outl"],position=position_dodge(.55)) +
+         geom_point(size = rel(2),shape = 22, colour = bar_cols["outl"],position=position_dodge(.55)) +
+         labs(x="", title = "Top terms, coherence and prevalence per topic")+
+         scale_y_continuous("", expand = c(0,0.01)) + 
+         scale_fill_manual("",values = bar_cols) +
+         theme(legend.position = "bottom") +
+         coord_flip()
+)
